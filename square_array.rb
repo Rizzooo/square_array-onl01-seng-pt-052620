61db01bdfb2e70277966fcaca52af51cd862c938
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-03 18:40:25 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  numbers.each do |num|
+  numbers.each do |number|
+    puts "Calling number: #{number}!"
+  numbers.each do |n|
+    numbers << #{n} ** 2
+    numbers
+  end
+  end
+  end
 end
